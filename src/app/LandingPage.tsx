7a419f92b1b5abcb79e02121c2c9df697f5148d4
Nico Betz <nico.b1102@icloud.com>
2025-11-25 14:02:34 +0100

    add routing

---
+++ src/app/LandingPage.tsx
@@ -0,0 +1,16 @@
+import './styles/App.css'
+
+function LandingPage() {
+  const handleLogin = () => {
+
+  }
+
+  return (
+    <>
+      login page
+      <button onClick={handleLogin}>Login</button>
+    </>
+  )
+}
+
+export default LandingPage
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
+import { Route, Routes } from 'react-router-dom';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './app/App.tsx'
+import LandingPage from './app/LandingPage.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+    </Routes>
+    <LandingPage />
   </StrictMode>,
 )
