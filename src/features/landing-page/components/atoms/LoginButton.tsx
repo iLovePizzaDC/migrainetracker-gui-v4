5cfa7d06828d59f5d4a59eefdcd7a2f038b7450a
Nico Betz <nico.b1102@icloud.com>
2025-11-26 18:28:42 +0100

    reformat code

--- src/features/landing-page/components/atoms/LoginButton.tsx
+++ src/features/landing-page/components/atoms/LoginButton.tsx
@@ -1,6 +1,6 @@
 import { fetchOAuthAccessToken } from "../../../../shared/api/google.api";
 
-export default function LoginButton() {
+function LoginButton() {
     const redirectToLogin = () => {
         const autoLogin = !!document.cookie.match(/MT_session=/);
         window.location.href = fetchOAuthAccessToken(autoLogin);
@@ -10,3 +10,5 @@ export default function LoginButton() {
         <button onClick={redirectToLogin} className='fixed border border-white p-4 m-7 right-0 bottom-0 rounded-xl z-30'>Login</button>
     );
 }
+
+export default LoginButton;
