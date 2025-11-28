677b486fa770dfdec3010d7e67871a3a0670c0c3
Nico Betz <nico.b1102@icloud.com>
2025-11-28 18:01:05 +0100

    add dummy calendar

--- src/features/calendar/pages/CalendarPage.tsx
+++ src/features/calendar/pages/CalendarPage.tsx
@@ -1,11 +1,16 @@
 import { useAuthCheck } from "../../../shared/auth/use-auth-check";
 import { useUser } from "../../../shared/hooks/user/use-user";
+import Calendar from "../components/organisms/Calendar";
 
 function CalendarPage() {
   const { user, setUser } = useUser();
   useAuthCheck(user, setUser);
 
-  return <>calendar page</>;
+  return (
+    <div>
+      <Calendar />
+    </div>
+  );
 }
 
 export default CalendarPage;
