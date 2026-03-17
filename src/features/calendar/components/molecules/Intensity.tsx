2744ef9f75a9e8aea3c8889b2ea426909ec113ba
Nico Betz <nico.b1102@icloud.com>
2026-03-17 08:34:26 +0100

    feat: add vi tests

--- src/features/calendar/components/molecules/Intensity.tsx
+++ src/features/calendar/components/molecules/Intensity.tsx
@@ -10,7 +10,7 @@ interface IIntensity {
 
 function Intensity({ intensity, setIntensity, disabled = false }: IIntensity) {
 	return (
-		<div className='p-4 rounded-xl bg-white/5 border border-white/10'>
+		<div data-testid='intensity' className='p-4 rounded-xl bg-white/5 border border-white/10'>
 			<h3 className='text-sm font-medium text-purple-300 mb-2'>Intensity</h3>
 
 			<SelectInput
