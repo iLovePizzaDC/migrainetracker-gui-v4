3f6a9edb03fd9ee461e3a7d8d81f420f46844ff7
Nico Betz <nico.b1102@icloud.com>
2026-03-24 17:47:47 +0100

    fix: auto redirect

--- src/features/calendar/pages/CalendarPage.tsx
+++ src/features/calendar/pages/CalendarPage.tsx
@@ -1,12 +1,7 @@
 import Calendar from '@/features/calendar/components/organisms/Calendar';
 import { CalendarProvider } from '@/features/calendar/hooks/calendar-provider';
-import { useAuthCheck } from '@/shared/auth/use-auth-check';
-import { useUser } from '@/shared/hooks/user/use-user';
 
 function CalendarPage() {
-	const { user, setUser } = useUser();
-	useAuthCheck(user, setUser);
-
 	return (
 		<div className='w-full'>
 			<div className='max-w-6xl mx-auto'>
