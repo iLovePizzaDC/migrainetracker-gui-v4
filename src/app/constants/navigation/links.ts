4bf00381f0174c8e06152bd579418cc2af2e2fd9
Nico <45760506+iLovePizzaDC@users.noreply.github.com>
2026-04-01 17:22:49 +0200

    fix: show different navigation when user is logged out (#34)
    * fix: show different navigation when user is logged out
    
    * fix: show different navigation when user is logged out
    
    * fix: show different navigation when user is logged out
    
    * Apply suggestion from @iLovePizzaDC

--- src/app/constants/navigation/links.ts
+++ src/app/constants/navigation/links.ts
@@ -1,13 +1,22 @@
 import type { NavigationLink } from '@/app/types/navigation/navigation';
 
 export const NAV_LINK_STATES = {
-	HOME: 'Home',
-	CALENDAR: 'Calendar',
+	HOME: {
+		label: 'Home',
+		allowAnonymous: false,
+	},
+	CALENDAR: {
+		label: 'Calendar',
+		allowAnonymous: false,
+	},
 } as const;
 
-export type NAV_LINK = (typeof NAV_LINK_STATES)[keyof typeof NAV_LINK_STATES];
+export type NAV_LINK = (typeof NAV_LINK_STATES)[keyof typeof NAV_LINK_STATES]['label'];
 
-export const NAVIGATION_LINKS: NavigationLink[] = Object.values(NAV_LINK_STATES).map((label) => ({
-	label,
-	to: `/${label.toLowerCase()}`,
-}));
+export const NAVIGATION_LINKS: NavigationLink[] = Object.values(NAV_LINK_STATES).map(
+	({ label, allowAnonymous }) => ({
+		label,
+		to: `/${label.toLowerCase()}`,
+		allowAnonymous,
+	}),
+);
