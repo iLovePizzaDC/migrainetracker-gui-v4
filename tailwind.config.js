3e80381f9332942f73a34ad5e107a4e3e87a324a
Nico <45760506+iLovePizzaDC@users.noreply.github.com>
2026-04-01 18:16:14 +0200

    feat: add logging in loading state (#32)
    * feat: add logging in loading state
    
    * feat: add logging in loading state
    
    * feat: add logging in loading state

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
 	content: ['./index.html', './src/**/*.{ts,tsx,js,jsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			keyframes: {
+				shimmer: {
+					'0%': { width: '0%', marginLeft: '0%' },
+					'50%': { width: '60%', marginLeft: '20%' },
+					'100%': { width: '0%', marginLeft: '100%' },
+				},
+			},
+		},
 	},
 	plugins: [],
 };
