260185ef1fb8ee68acf27292f72251c81302e237
Nico <45760506+iLovePizzaDC@users.noreply.github.com>
2026-04-01 17:22:49 +0200

    fix: show different navigation when user is logged out (#34)
    * fix: show different navigation when user is logged out
    
    * fix: show different navigation when user is logged out
    
    * fix: show different navigation when user is logged out
    
    * Apply suggestion from @iLovePizzaDC

--- src/app/components/molecules/navigation/MobileNavigationLinks.tsx
+++ src/app/components/molecules/navigation/MobileNavigationLinks.tsx
@@ -1,23 +1,28 @@
 import NavigationLink from '@/app/components/atoms/navigation/NavigationLink';
 import { NAVIGATION_LINKS } from '@/app/constants/navigation/links';
+import { useUser } from '@/shared/hooks/user/use-user';
 
 interface IMobileNavigationLinks {
 	toggleMenu: () => void;
 }
 
 export default function MobileNavigationLinks({ toggleMenu }: IMobileNavigationLinks) {
+	const { user } = useUser();
+
 	return (
 		<div className='space-y-3'>
-			{NAVIGATION_LINKS.map((navigationLink) => (
-				<div key={`mobilenavigationlink-${navigationLink.to}`}>
-					<NavigationLink
-						label={navigationLink.label}
-						to={navigationLink.to}
-						onClick={toggleMenu}
-						className='block text-sm pl-4 p-2 rounded-full'
-					/>
-				</div>
-			))}
+			{NAVIGATION_LINKS.filter(({ allowAnonymous }) => allowAnonymous || user !== null).map(
+				(navigationLink) => (
+					<div key={`mobilenavigationlink-${navigationLink.to}`}>
+						<NavigationLink
+							label={navigationLink.label}
+							to={navigationLink.to}
+							onClick={toggleMenu}
+							className='block text-sm pl-4 p-2 rounded-full'
+						/>
+					</div>
+				),
+			)}
 		</div>
 	);
 }
